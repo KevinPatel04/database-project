7417d995f779c810e27ec0f59a095ba0f35793d0
Kevin Patel <patelkvin04@gmail.com>
2021-12-01 23:25:25 -0500

    Update exam_view.py

--- src/pages/exam_view.py
+++ src/pages/exam_view.py
@@ -35,9 +35,9 @@ def write():
                             exams = conn.query_db_all(sql_table)
                             exams_eid = exams['eid'].tolist()
                             with col3:
-                                exam = st.selectbox("Choose a exam", exams_eid)
-                            if exam and term and course:
-                                sql_table = f"SELECT * FROM questions WHERE eid = {exam};"
+                                exam_eid = st.selectbox("Choose a exam", exams_eid)
+                            if exam_eid and term and course:
+                                sql_table = f"SELECT * FROM questions WHERE eid = {exam_eid};"
                                 try:
                                     df = conn.query_db_all(sql_table)
                                 except:
@@ -45,15 +45,23 @@ def write():
                                 else:
                                     # st.dataframe(df)
                                     cols = st.columns(3)
-                                    select_managed_by = f"SELECT firstname || ' ' || lastname AS name FROM users WHERE uid = {exam['managed_by']} LIMIT 1;"
+                                    select_exam_meta = f"SELECT firstname || ' ' || lastname AS name,managed_by,is_released,due_date,due_time FROM users, exam WHERE uid = managed_by AND eid={exam_eid} LIMIT 1;"
                                     try:
-                                        managed_by = conn.query_db_all(select_managed_by).iloc[0]['name']
+                                        exam_meta = conn.query_db_all(select_exam_meta).iloc[0]
                                     except:
                                         st.write("Sorry! Something went wrong with your query, please try again.")
                                     else:
-                                        st.markdown(f"**Exam Managed By: {managed_by}**")
-                                        if exams['is_released']:
-                                            pass
+                                        with cols[0]:
+                                            st.markdown("**Exam Managed By:**")
+                                            st.markdown(f"**{exam_meta['name']}**")
+                                        with cols[1]:
+                                            st.markdown(f"**Due:**")
+                                            st.markdown(f"**{exam_meta['due_date']} {exam_meta['due_time']}**")
+                                        with cols[2]:
+                                            if exam_meta['is_released']:
+                                                st.success('Released')
+                                            else:
+                                                st.warning('Not Released')
                                         st.markdown("---")
                                         for idx in range(len(df)):
                                             create_question_card(df,idx)
